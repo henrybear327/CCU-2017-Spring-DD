769fe71ff8be86b9dec1bb6c4296d170929b2a11
Henry Tseng <henrybear327@gmail.com>
2017-04-17 18:45:49 +0800

    Fix 16-bit-cla

--- 6/CLA_16bit/CLA_16bit.v
+++ 6/CLA_16bit/CLA_16bit.v
@@ -13,7 +13,10 @@ module cla_16bit( a , b , cin , s , cout);
 	wire [3:0] c_4_8_12;
 	//write your design below
 
-	gp_generator cla16_pg(.a(a), .b(b), .p(p), .g(g));
+	gp_generator cla4_pg1(.a(a[3:0]), .b(b[3:0]), .p(p[3:0]), .g(g[3:0]));
+	gp_generator cla4_pg2(.a(a[7:4]), .b(b[7:4]), .p(p[7:4]), .g(g[7:4]));
+	gp_generator cla4_pg3(.a(a[11:8]), .b(b[11:8]), .p(p[11:8]), .g(g[11:8]));
+	gp_generator cla4_pg4(.a(a[15:12]), .b(b[15:12]), .p(p[15:12]), .g(g[15:12]));
 
 	carry_generator carry_gen_1(.p(p[3:0]) , .g(g[3:0]) , .cin(cin) , .c(c[3:0]) , .gG(gG[0]), .gP(gP[0]));
 	carry_generator carry_gen_2(.p(p[7:4]) , .g(g[7:4]) , .cin(c_4_8_12[1]) , .c(c[7:4]) , .gG(gG[1]), .gP(gP[1]));
@@ -21,15 +24,18 @@ module cla_16bit( a , b , cin , s , cout);
 	carry_generator carry_gen_4(.p(p[15:12]) , .g(g[15:12]) , .cin(c_4_8_12[3]) , .c(c[15:12]) , .gG(gG[3]), .gP(gP[3]));
 	carry_generator carry_gen_5(.p(gP) , .g(gG) , .cin(cin) , .c(c_4_8_12) , .cout(cout));
 
-	sum_generator cla16_sum(.a(a), .b(b), .c(c), .s(s));
+	sum_generator cla4_sum1(.a(a[3:0]), .b(b[3:0]), .c(c[3:0]), .s(s[3:0]));
+	sum_generator cla4_sum2(.a(a[7:4]), .b(b[7:4]), .c(c[7:4]), .s(s[7:4]));
+	sum_generator cla4_sum3(.a(a[11:8]), .b(b[11:8]), .c(c[11:8]), .s(s[11:8]));
+	sum_generator cla4_sum4(.a(a[15:12]), .b(b[15:12]), .c(c[15:12]), .s(s[15:12]));
 
 endmodule
 
 
 //產生所需的小p小g
 module gp_generator(a, b, p, g );
-     input [15:0] a,b;
-	 output [15:0] p,g;
+     input [3:0] a,b;
+	 output [3:0] p,g;
 
 	 assign g[0] = a[0] & b[0];
 	 assign p[0] = a[0] | b[0];
@@ -42,42 +48,6 @@ module gp_generator(a, b, p, g );
 
 	 assign g[3] = a[3] & b[3];
 	 assign p[3] = a[3] | b[3];
-
-	 assign g[4] = a[4] & b[4];
-	 assign p[4] = a[4] | b[4];
-
-	 assign g[5] = a[5] & b[5];
-	 assign p[5] = a[5] | b[5];
-
-	 assign g[6] = a[6] & b[6];
-	 assign p[6] = a[6] | b[6];
-
-	 assign g[7] = a[7] & b[7];
-	 assign p[7] = a[7] | b[7];
-
-	 assign g[8] = a[8] & b[8];
-	 assign p[8] = a[8] | b[8];
-
-	 assign g[9] = a[9] & b[9];
-	 assign p[9] = a[9] | b[9];
-
-	 assign g[10] = a[10] & b[10];
-	 assign p[10] = a[10] | b[10];
-
-	 assign g[11] = a[11] & b[11];
-	 assign p[11] = a[11] | b[11];
-
-	 assign g[12] = a[12] & b[12];
-	 assign p[12] = a[12] | b[12];
-
-	 assign g[13] = a[13] & b[13];
-	 assign p[13] = a[13] | b[13];
-
-	 assign g[14] = a[14] & b[14];
-	 assign p[14] = a[14] | b[14];
-
-	 assign g[15] = a[15] & b[15];
-	 assign p[15] = a[15] | b[15];
 
 endmodule
 
@@ -104,24 +74,12 @@ endmodule
 //求得最後結果
 module sum_generator(a, b, c, s );
 
-     input [15:0] a,b,c;
-	 output [15:0] s;
+     input [3:0] a,b,c;
+	 output [3:0] s;
 
 	 assign s[0] = a[0] ^ b[0] ^ c[0];
 	 assign s[1] = a[1] ^ b[1] ^ c[1];
 	 assign s[2] = a[2] ^ b[2] ^ c[2];
 	 assign s[3] = a[3] ^ b[3] ^ c[3];
-	 assign s[4] = a[4] ^ b[4] ^ c[4];
-	 assign s[5] = a[5] ^ b[5] ^ c[5];
-	 assign s[6] = a[6] ^ b[6] ^ c[6];
-	 assign s[7] = a[7] ^ b[7] ^ c[7];
-	 assign s[8] = a[8] ^ b[8] ^ c[8];
-	 assign s[9] = a[9] ^ b[9] ^ c[9];
-	 assign s[10] = a[10] ^ b[10] ^ c[10];
-	 assign s[11] = a[11] ^ b[11] ^ c[11];
-	 assign s[12] = a[12] ^ b[12] ^ c[12];
-	 assign s[13] = a[13] ^ b[13] ^ c[13];
-	 assign s[14] = a[14] ^ b[14] ^ c[14];
-	 assign s[15] = a[15] ^ b[15] ^ c[15];
 
 endmodule
