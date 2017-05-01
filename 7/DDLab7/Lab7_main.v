5caf7ac8f1f52407f64175b4d6faab8693c97411
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-05-01 06:10:04 -0500

    Add files via upload

--- 7/DDLab7/Lab7_main.v
+++ 7/DDLab7/Lab7_main.v
@@ -16,10 +16,10 @@ module Lab7_main(
 	always@(posedge clk)
 	begin
 		case({rst, opa, ops, opm}) 
-			2'b1000 : op=2'b00; 
-			2'b0100 : op=2'b01; 
-            		2'b0010 : op=2'b10;
-            		2'b0001 : op=2'b11;
+			4'b1000 : op=2'b00; 
+			4'b0100 : op=2'b01; 
+         4'b0010 : op=2'b10;
+         4'b0001 : op=2'b11;
 		endcase 
 		case (op) 
 			2'b01 : result=a+b; 
