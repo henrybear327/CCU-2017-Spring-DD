ca2e363c953751dadfdd1e2210940aee9ffbc716
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-05-01 19:10:34 +0800

    Update Lab7_main.v

--- 7/DDLab7/Lab7_main.v
+++ 7/DDLab7/Lab7_main.v
@@ -18,8 +18,8 @@ module Lab7_main(
 		case({rst, opa, ops, opm}) 
 			4'b1000 : op=2'b00; 
 			4'b0100 : op=2'b01; 
-         4'b0010 : op=2'b10;
-         4'b0001 : op=2'b11;
+         		4'b0010 : op=2'b10;
+         		4'b0001 : op=2'b11;
 		endcase 
 		case (op) 
 			2'b01 : result=a+b; 
