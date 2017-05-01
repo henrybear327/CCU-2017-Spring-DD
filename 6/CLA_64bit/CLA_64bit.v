b4e887ffd03795807ced6eb85d91bc8685abcc0e
Henry Tseng <henrybear327@gmail.com>
2017-05-01 16:42:16 +0800

    still wrong

--- 6/CLA_64bit/CLA_64bit.v
+++ 6/CLA_64bit/CLA_64bit.v
@@ -1,144 +1,107 @@
 module cla_64bit( a, b, cin, sum, cout);
-
 	input [63:0] a, b;
-	input cin;
+    input cin;
+
 	output [63:0] sum;
 	output cout;
 
-
-	wire [15:0] c_4_8_12;
-
-    wire [63:0] p, g, s;
-    wire [63:0] c;
+	wire [63:0] p, g;
+	wire [16:0] c;
 	wire [4:0] c16;
-	wire [15:0] gG ,gP;
-	wire [3:0] g_G_16, g_P_16;
-	//write your design below
+	wire [15:0] gG_4,gP_4;
+	wire [3:0] gG_16,gP_16;
 
-    gp_generator cla4_pg1(.a(a[3:0]), .b(b[3:0]), .p(p[3:0]), .g(g[3:0]));
-	gp_generator cla4_pg2(.a(a[7:4]), .b(b[7:4]), .p(p[7:4]), .g(g[7:4]));
-	gp_generator cla4_pg3(.a(a[11:8]), .b(b[11:8]), .p(p[11:8]), .g(g[11:8]));
-	gp_generator cla4_pg4(.a(a[15:12]), .b(b[15:12]), .p(p[15:12]), .g(g[15:12]));
+    // 小 p 小 g
+	pg_generator pg1(a[3:0],    b[3:0],   p[3:0],   g[3:0]);
+	pg_generator pg2(a[7:4],    b[7:4],   p[7:4],   g[7:4]);
+	pg_generator pg3(a[11:8],   b[11:8],  p[11:8],  g[11:8]);
+	pg_generator pg4(a[15:12],  b[15:12], p[15:12], g[15:12]);
+	pg_generator pg5(a[19:16],  b[19:16], p[19:16], g[19:16]);
+	pg_generator pg6(a[23:20],  b[23:20], p[23:20], g[23:20]);
+	pg_generator pg7(a[27:24],  b[27:24], p[27:24], g[27:24]);
+	pg_generator pg8(a[31:28],  b[31:28], p[31:28], g[31:28]);
+	pg_generator pg9(a[35:32],  b[35:32], p[35:32], g[35:32]);
+	pg_generator pg10(a[39:36], b[39:36], p[39:36], g[39:36]);
+	pg_generator pg11(a[43:40], b[43:40], p[43:40], g[43:40]);
+	pg_generator pg12(a[47:44], b[47:44], p[47:44], g[47:44]);
+	pg_generator pg13(a[51:48], b[51:48], p[51:48], g[51:48]);
+	pg_generator pg14(a[55:52], b[55:52], p[55:52], g[55:52]);
+	pg_generator pg15(a[59:56], b[59:56], p[59:56], g[59:56]);
+	pg_generator pg16(a[63:60], b[63:60], p[63:60], g[63:60]);
 
-    gp_generator cla4_pg5(.a(a[19:16]), .b(b[19:16]), .p(p[19:16]), .g(g[19:16]));
-	gp_generator cla4_pg6(.a(a[23:20]), .b(b[23:20]), .p(p[23:20]), .g(g[23:20]));
-	gp_generator cla4_pg7(.a(a[27:24]), .b(b[27:24]), .p(p[27:24]), .g(g[27:24]));
-	gp_generator cla4_pg8(.a(a[31:28]), .b(b[31:28]), .p(p[31:28]), .g(g[31:28]));
+    // 四個一組的PG會算出來，小c也會
+	group_CLA_4bit carry_generator1 (p[3:0],  g[3:0] ,  cin ,  gG_4[0] , gP_4[0] , , sum[3:0]);
+	group_CLA_4bit carry_generator2 (p[7:4],  g[7:4] ,  c[1] , gG_4[1] , gP_4[1] , , sum[7:4]);
+    group_CLA_4bit carry_generator3 (p[11:8], g[11:8] , c[2] , gG_4[2] , gP_4[2] , , sum[11:8]);
+	group_CLA_4bit carry_generator4 (p[15:12],g[15:12] ,c[3] , gG_4[3] , gP_4[3] , , sum[15:12]);
+	group_CLA_4bit carry_generator5 (p[19:16],g[19:16] ,c[4] , gG_4[4] , gP_4[4] , , sum[19:16]);
+	group_CLA_4bit carry_generator6 (p[23:20],g[23:20] ,c[5] , gG_4[5] , gP_4[5] , , sum[23:20]);
+	group_CLA_4bit carry_generator7 (p[27:24],g[27:24] ,c[6] , gG_4[6] , gP_4[6] , , sum[27:24]);
+	group_CLA_4bit carry_generator8 (p[31:28],g[31:28] ,c[7] , gG_4[7] , gP_4[7] , , sum[31:28]);
+	group_CLA_4bit carry_generator9 (p[35:32],g[35:32] ,c[8] , gG_4[8] , gP_4[8] , , sum[35:32]);
+	group_CLA_4bit carry_generator10(p[39:36],g[39:36] ,c[9] , gG_4[9] , gP_4[9] , , sum[39:36]);
+	group_CLA_4bit carry_generator11(p[43:40],g[43:40] ,c[10] ,gG_4[10] ,gP_4[10] , ,sum[43:40]);
+	group_CLA_4bit carry_generator12(p[47:44],g[47:44] ,c[11] ,gG_4[11] ,gP_4[11] , ,sum[47:44]);
+	group_CLA_4bit carry_generator13(p[51:48],g[51:48] ,c[12] ,gG_4[12] ,gP_4[12] , ,sum[51:48]);
+    group_CLA_4bit carry_generator14(p[55:52],g[55:52] ,c[13] ,gG_4[13] ,gP_4[13] , ,sum[55:52]);
+	group_CLA_4bit carry_generator15(p[59:56],g[59:56] ,c[14] ,gG_4[14] ,gP_4[14] , ,sum[59:56]);
+	group_CLA_4bit carry_generator16(p[63:60],g[63:60] ,c[15] ,gG_4[15] ,gP_4[15] , ,sum[63:60]);
 
-    gp_generator cla4_pg9(.a(a[35:32]), .b(b[35:32]), .p(p[35:32]), .g(g[35:32]));
-	gp_generator cla4_pg10(.a(a[39:36]), .b(b[39:36]), .p(p[39:36]), .g(g[39:36]));
-	gp_generator cla4_pg11(.a(a[43:40]), .b(b[43:40]), .p(p[43:40]), .g(g[43:40]));
-	gp_generator cla4_pg12(.a(a[47:44]), .b(b[47:44]), .p(p[47:44]), .g(g[47:44]));
+    // 16個一組的 P, G
+	group_CLA_4bit carry_generator17(gP_4[3:0],  gG_4[3:0] , ,  gG_16[0] ,gP_16[0] , ,);
+	group_CLA_4bit carry_generator18(gP_4[7:4],  gG_4[7:4] , ,  gG_16[1] ,gP_16[1] , ,);
+	group_CLA_4bit carry_generator19(gP_4[11:8], gG_4[11:8] , , gG_16[2] ,gP_16[2] , ,);
+	group_CLA_4bit carry_generator20(gP_4[15:12],gG_4[15:12] , ,gG_16[3] ,gP_16[3] , ,);
 
-    gp_generator cla4_pg13(.a(a[51:48]), .b(b[51:48]), .p(p[51:48]), .g(g[51:48]));
-	gp_generator cla4_pg14(.a(a[55:52]), .b(b[55:52]), .p(p[55:52]), .g(g[55:52]));
-	gp_generator cla4_pg15(.a(a[59:56]), .b(b[59:56]), .p(p[59:56]), .g(g[59:56]));
-	gp_generator cla4_pg16(.a(a[63:60]), .b(b[63:60]), .p(p[63:60]), .g(g[63:60]));
+    // overflow bit
+    group_CLA_4bit carry_generator21(gP_16[3:0],gG_16[3:0] ,cin , , ,c16[4:0] ,);
 
-	carry_generator carry_gen_1(.p(p[3:0]) , .g(g[3:0]) , .cin(cin) , .c(c[3:0]) , .gG(gG[0]), .gP(gP[0]));
-	carry_generator carry_gen_2(.p(p[7:4]) , .g(g[7:4]) , .cin(c_4_8_12[1]) , .c(c[7:4]) , .gG(gG[1]), .gP(gP[1]));
-	carry_generator carry_gen_3(.p(p[11:8]) , .g(g[11:8]) , .cin(c_4_8_12[2]) , .c(c[11:8]) , .gG(gG[2]), .gP(gP[2]));
-	carry_generator carry_gen_4(.p(p[15:12]) , .g(g[15:12]) , .cin(c_4_8_12[3]) , .c(c[15:12]) , .gG(gG[3]), .gP(gP[3]));
-
-    carry_generator carry_gen_5(.p(p[19:16]), .g(g[19:16]) , .cin(c_4_8_12[4]) , .c(c[19:16]) , .gG(gG[4]), .gP(gP[4]));
-	carry_generator carry_gen_6(.p(p[23:20]), .g(g[23:20]) , .cin(c_4_8_12[5]) , .c(c[23:20]) , .gG(gG[5]), .gP(gP[5]));
-	carry_generator carry_gen_7(.p(p[27:24]), .g(g[27:24]) , .cin(c_4_8_12[6]) , .c(c[27:24]) , .gG(gG[6]), .gP(gP[6]));
-	carry_generator carry_gen_8(.p(p[31:28]), .g(g[31:28]) , .cin(c_4_8_12[7]) , .c(c[31:28]) , .gG(gG[7]), .gP(gP[7]));
-
-    carry_generator carry_gen_9(.p(p[35:32]), .g(g[35:32])  , .cin(c_4_8_12[8]) , .c(c[35:32]) , .gG(gG[8]), .gP(gP[8]));
-    carry_generator carry_gen_10(.p(p[39:36]), .g(g[39:36]) , .cin(c_4_8_12[9]) , .c(c[39:36]) , .gG(gG[9]), .gP(gP[9]));
-    carry_generator carry_gen_11(.p(p[43:40]), .g(g[43:40]) , .cin(c_4_8_12[10]) , .c(c[43:40]) , .gG(gG[10]), .gP(gP[10]));
-    carry_generator carry_gen_12(.p(p[47:44]), .g(g[47:44]) , .cin(c_4_8_12[11]) , .c(c[47:44]) , .gG(gG[11]), .gP(gP[11]));
-
-    carry_generator carry_gen_13(.p(p[51:48]), .g(g[51:48]) , .cin(c_4_8_12[12]) , .c(c[51:48]) , .gG(gG[12]), .gP(gP[12]));
-    carry_generator carry_gen_14(.p(p[55:52]), .g(g[55:52]) , .cin(c_4_8_12[13]) , .c(c[55:52]) , .gG(gG[13]), .gP(gP[13]));
-    carry_generator carry_gen_15(.p(p[59:56]), .g(g[59:56]) , .cin(c_4_8_12[14]) , .c(c[59:56]) , .gG(gG[14]), .gP(gP[14]));
-    carry_generator carry_gen_16(.p(p[63:60]), .g(g[63:60]) , .cin(c_4_8_12[15]) , .c(c[63:60]) , .gG(gG[15]), .gP(gP[15]));
-
-    carry_generator carry_gen_17(gP[3:0] , gG[3:0]     ,  , , g_G_16[0], g_P_16[0], );
-    carry_generator carry_gen_18(gP[7:4] , gG[7:4]     ,  , , g_G_16[1], g_P_16[1], );
-    carry_generator carry_gen_19(gP[11:8] , gG[11:8]   ,  , , g_G_16[2], g_P_16[2], );
-    carry_generator carry_gen_20(gP[15:12] , gG[15:12] ,  , , g_G_16[3], g_P_16[3], );
-
-    carry_generator carry_gen_21(g_P_16[3:0] , g_G_16[3:0] , cin , c16[3:0] , , , );
-
-    carry_generator carry_gen_22(gP[3:0]   , gG[3:0]   , c16[0] , c[3:0]  , , ,  );
-    carry_generator carry_gen_23(gP[7:4]   , gG[7:4]   , c16[1] , c[7:4]  , , , );
-    carry_generator carry_gen_24(gP[11:8]  , gG[11:8]  , c16[2] , c[11:8] , , , );
-    carry_generator carry_gen_25(gP[15:12] , gG[15:12] , c16[3] , c[15:12], , , );
-
-
-
-	sum_generator cla4_sum1(a[3:0], b[3:0], c[3:0], s[3:0]);
-	sum_generator cla4_sum2(a[7:4], b[7:4], c[7:4], s[7:4]);
-	sum_generator cla4_sum3(a[11:8], b[11:8], c[11:8], s[11:8]);
-	sum_generator cla4_sum4(a[15:12], b[15:12], c[15:12], s[15:12]);
-
-    sum_generator cla4_sum5(a[19:16], b[19:16], c[19:16], s[19:16]);
-    sum_generator cla4_sum6(a[23:20], b[23:20], c[23:20], s[23:20]);
-    sum_generator cla4_sum7(a[27:24], b[27:24], c[27:24], s[27:24]);
-    sum_generator cla4_sum8(a[31:28], b[31:28], c[31:28], s[31:28]);
-
-    sum_generator cla4_sum9(a[35:32], b[35:32], c[35:32], s[35:32]);
-    sum_generator cla4_sum10(a[39:36], b[39:36], c[39:36], s[39:36]);
-    sum_generator cla4_sum11(a[43:40], b[43:40], c[43:40], s[43:40]);
-    sum_generator cla4_sum12(a[47:44], b[47:44], c[47:44], s[47:44]);
-
-    sum_generator cla4_sum13(a[51:48], b[51:48], c[51:48], s[51:48]);
-    sum_generator cla4_sum14(a[55:52], b[55:52], c[55:52], s[55:52]);
-    sum_generator cla4_sum15(a[59:56], b[59:56], c[59:56], s[59:56]);
-    sum_generator cla4_sum16(a[63:60], b[63:60], c[63:60], s[63:60]);
-
+	group_CLA_4bit carry_generator22(gP_4[3:0],  gG_4[3:0] ,  c16[0] , , ,c[4:0] ,);
+	group_CLA_4bit carry_generator23(gP_4[7:4],  gG_4[7:4] ,  c16[1] , , ,c[8:4] ,);
+	group_CLA_4bit carry_generator24(gP_4[11:8], gG_4[11:8] , c16[2] , , ,c[12:8] ,);
+	group_CLA_4bit carry_generator25(gP_4[15:12],gG_4[15:12] ,c16[3] , , ,c[16:12] ,);
 endmodule
 
-//產生所需的小p小g
-module gp_generator(a, b, p, g );
-     input [3:0] a,b;
-	 output [3:0] p,g;
-
-	 assign g[0] = a[0] & b[0];
-	 assign p[0] = a[0] | b[0];
-
-	 assign g[1] = a[1] & b[1];
-	 assign p[1] = a[1] | b[1];
-
-	 assign g[2] = a[2] & b[2];
-	 assign p[2] = a[2] | b[2];
-
-	 assign g[3] = a[3] & b[3];
-	 assign p[3] = a[3] | b[3];
-
-endmodule
-
-//產生所需的carry in 跟 大G大P
-module carry_generator( p , g , cin , c ,gG ,gP ,cout);
+module group_CLA_4bit( p, g, cin, gG, gP, c, sum);
 
 	input [3:0] p, g;
 	input cin;
-	output [3:0] c;
 	output gG, gP;
-	output cout;
+	output [4:0] c;
+	output	[3:0] sum;
 
-	assign gG = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]);//G[3:0]
-	assign gP = p[3] & p[2] & p[1] & p[0];//P[3:0]
+	assign gG = (g[0] & p[1] & p[2] & p[3])|(g[1]&p[2]&p[3])|(g[2]&p[3])|g[3];
+	assign gP = p[0] & p[1] & p[2] & p[3];
 
 	assign c[0] = cin;
-	assign c[1] = g[0] | (p[0] & cin);
-	assign c[2] = g[1] | (p[1] & g[0]) | (p[1] & p[0] & cin);
-	assign c[3] = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & cin);
-	assign cout = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]) | (p[3] & p[2] & p[1] & p[0] & cin);
+	assign c[1] = g[0]|(p[0] & cin);
+	assign c[2] = g[1]|(p[1] & g[0])|(p[1] & p[0] & cin);
+	assign c[3] = g[2]|(p[2] & g[1])|(p[2] & p[1] & g[0])|(p[2] & p[1] & p[0] & cin);
+	assign c[4] = g[3]|(p[3] & g[2])|(p[3] & p[2] & g[1])|(p[3] & p[2] & p[1] & g[0])|(p[3] & p[2] & p[1] & p[0] & cin);
+
+	assign sum[0] = c[0] ^ p[0]; // c[0] ^ a[0] ^ b[0]
+	assign sum[1] = c[1] ^ p[1]; // c[1] ^ a[1] ^ b[1]
+	assign sum[2] = c[2] ^ p[2]; // c[2] ^ a[2] ^ b[2]
+	assign sum[3] = c[3] ^ p[3]; // c[3] ^ a[3] ^ b[3]
 
 endmodule
 
-//求得最後結果
-module sum_generator(a, b, c, s );
 
-     input [3:0] a,b,c;
-	 output [3:0] s;
+module pg_generator( a, b, p, g);
 
-	 assign s[0] = a[0] ^ b[0] ^ c[0];
-	 assign s[1] = a[1] ^ b[1] ^ c[1];
-	 assign s[2] = a[2] ^ b[2] ^ c[2];
-	 assign s[3] = a[3] ^ b[3] ^ c[3];
+        input [3:0] a, b;
+        output [3:0] p, g;
+
+        // Carry Propagate
+        assign p[0] = a[0] ^ b[0] ;
+        assign p[1] = a[1] ^ b[1] ;
+        assign p[2] = a[2] ^ b[2] ;
+        assign p[3] = a[3] ^ b[3] ;
+
+        //carry Generate
+        assign g[0] = a[0] & b[0] ;
+        assign g[1] = a[1] & b[1] ;
+	    assign g[2] = a[2] & b[2] ;
+        assign g[3] = a[3] & b[3] ;
 
 endmodule
